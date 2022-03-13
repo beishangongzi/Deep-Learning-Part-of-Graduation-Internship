f2d02099c31ab1eaf2e5d43e8f065460d0ec7f0b
Andy Z Wright <zhangruibin021@gmail.com>
2022-03-13 19:04:51 +0800

    讲默认参数从main中迁移至.env文件中

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ from predict import predict_report
 from training import training
 from visual_show import predict_visuality
 
-FLAGS = tf.compat.v1.flags.FLAGS
-
-tf.compat.v1.flags.DEFINE_string('pre_model', "tf2-preview_mobilenet_v2_classification_4", 'the pre model')
-tf.compat.v1.flags.DEFINE_string("model", "mobile_v3_transfer_model", "model that used")
-tf.compat.v1.flags.DEFINE_bool("train", True, "train a new model or use saved model")
-
 
 def begin_main():
     if not os.path.exists("reports"):
@@ -45,4 +39,10 @@ def main(unused_argv):
 
 
 if __name__ == '__main__':
+    FLAGS = tf.compat.v1.flags.FLAGS
+
+    tf.compat.v1.flags.DEFINE_string('pre_model', os.getenv("model"), 'the pre model')
+    tf.compat.v1.flags.DEFINE_string("model", os.getenv("pre_model"), "model that used")
+    tf.compat.v1.flags.DEFINE_bool("train", False if os.getenv("train") == "False" else True,
+                                   "train a new model or use saved model")
     tf.compat.v1.app.run()
