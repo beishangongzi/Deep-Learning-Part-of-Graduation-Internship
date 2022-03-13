037a491f36c10b469f439030fdf5811205c560cf
Andy Z Wright <zhangruibin021@gmail.com>
2022-03-13 13:40:22 +0800

    add flag train

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ FLAGS = tf.compat.v1.flags.FLAGS
 
 tf.compat.v1.flags.DEFINE_string('pre_model', "tf2-preview_mobilenet_v2_classification_4", 'the pre model')
 tf.compat.v1.flags.DEFINE_string("model", "mobile_v3_transfer_model", "model that used")
+tf.compat.v1.flags.DEFINE_bool("train", True, "train a new model or use saved model")
 
 def begin_main():
     if not os.path.exists("reports"):
@@ -29,9 +30,13 @@ def begin_main():
 def main(unused_argv):
     load_dotenv()
     begin_main()
-    model = eval(FLAGS.model)(FLAGS.pre_model)
-    dataset = pathlib.Path(os.getenv("data_root"))
-    export_path = training(model, dataset)
+    if FLAGS.train:
+        model = eval(FLAGS.model)(FLAGS.pre_model)
+
+        dataset = pathlib.Path(os.getenv("data_root"))
+        export_path = training(model, dataset)
+    else:
+        export_path = os.path.join("model", FLAGS.model)
     predict_report(export_path, pathlib.Path(os.getenv("test_dir")))
     predict_visuality(export_path, pathlib.Path(os.getenv("test_dir")))
 
