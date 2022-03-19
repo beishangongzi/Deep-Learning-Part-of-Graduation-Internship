551b73457012f0ea77399436daf54d1145defd9d
Andy Z Wright <zhangruibin021@gmail.com>
2022-03-19 19:11:51 +0800

    fix data type

--- data_augmentation.py
+++ data_augmentation.py
@@ -6,8 +6,8 @@ from tensorflow import keras
 data_augmentation = keras.Sequential(
     [
         layers.RandomFlip("horizontal",
-                          input_shape=(os.getenv("img_height"),
-                                       os.getenv("img_width"),
+                          input_shape=(int(os.getenv("img_height")),
+                                       int(os.getenv("img_width")),
                                        3)),
         layers.RandomRotation(0.1),
         layers.RandomZoom(0.1),
