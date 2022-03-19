2297d1304a17fe31fc31f1f036fb1fcb2a900054
Andy Z Wright <zhangruibin021@gmail.com>
2022-03-19 19:05:18 +0800

    add data _augmentation

--- data_augmentation.py
+++ data_augmentation.py
@@ -1,30 +1,15 @@
+import os
+
+from keras import layers
 from tensorflow import keras
-import tensorflow as tf
 
-
-def random_invert_img(x, p=0.5):
-    if tf.random.uniform([]) < p:
-        x = (255 - x)
-    else:
-        x
-    return x
-
-
-def random_invert(factor=0.5):
-    return keras.layers.Lambda(lambda x: random_invert_img(x, factor))
-
-
-class RandomInvert(keras.layers.Layer):
-    def __init__(self, factor=0.5, **kwargs):
-        super().__init__(**kwargs)
-        self.factor = factor
-
-    def call(self, x):
-        return random_invert_img(x)
-
-
-data_augmentation = tf.keras.Sequential([
-    keras.layers.RandomFlip("horizontal_and_vertical"),
-    keras.layers.RandomRotation(0.2),
-    # RandomInvert()
-])
+data_augmentation = keras.Sequential(
+    [
+        layers.RandomFlip("horizontal",
+                          input_shape=(os.getenv("img_height"),
+                                       os.getenv("img_width"),
+                                       3)),
+        layers.RandomRotation(0.1),
+        layers.RandomZoom(0.1),
+    ]
+)
